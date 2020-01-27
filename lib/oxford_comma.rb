348bd6a5ad327e234284018407d8d232b10f6d2b
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-27 15:31:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,10 +4,14 @@ def oxford_comma(array)
     return "#{array[0]}"
   elsif array.length == 2
     return array.join(" and ")
-  elsif array.length >= 3
-  array[-1] = "and #{array[-1]}"
-    return array.join(", ")
- end
+ 
 
  
 
+
+
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
+end
