f2b83e5271b9bb3bc0c8c56a3920ecbc770cd8e0
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-27 15:32:12 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 1  
-    #return array
-    return "#{array[0]}"
+    return array
+    #return "#{array[0]}"
   elsif array.length == 2
     return array.join(" and ")
   elsif array.length >= 3
@@ -9,6 +9,3 @@ def oxford_comma(array)
     return array.join(", ")
  end
  end
-
- 
-
