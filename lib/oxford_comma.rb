baa8ce75788361e7d9210e367b52cb3ca18a0fdc
Daniel J Feleppa <dfeleppa@gmail.com>
2020-01-27 15:27:58 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,7 @@
 def oxford_comma(array)
   if array.length == 1  
     puts array
-  elsif array..length == 2
+  elsif array.length == 2
     array.join(" and ")
   else
   array[-1] = "and #{array[-1]}"
